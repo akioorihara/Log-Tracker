8d77fad4ebd7461b758b456f2d3d94704e798ca6
Akio <oriharaakio@yahoo.com>
2020-02-24 18:16:40 -0600

    trying to create a class for printing

--- logging.py
+++ logging.py
@@ -1,7 +1,15 @@
 import datetime, os, time, stat 
 
+
 now = datetime.datetime.now()
 
+
+#Class for print a file name  
+class DisplayFileName: 
+    def __inti__(name, number):
+    display.name = name 
+    display.number = number 
+    
 f = open('log.txt', 'a') #open an exiting file 
 f.write("Current time is : " + str(now) + "\n")
 
@@ -9,13 +17,15 @@ f.write("Current time is : " + str(now) + "\n")
 
 #Write a mod time of the log file
 FileCreated = os.stat('log.txt')
+FileName = 'log.txt'
+
 ModTime = time.ctime(FileCreated[stat.ST_MTIME])
-f.write(FileCreated + "Last Modified Time: " + ModTime + '\n\n')
+#f.write(FileCreated + "Last Modified Time: " + ModTime + '\n\n')
 
 #print(f.read())
 
 
-#print a file name func 
+
 
 
 
