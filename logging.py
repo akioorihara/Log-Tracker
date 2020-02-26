1d8b77c5ccba840801617e4d25b34742e7830f4f
Akio <oriharaakio@yahoo.com>
2020-02-25 18:22:03 -0600

    Trying to creat a class for display and print

--- logging.py
+++ logging.py
@@ -1,25 +1,30 @@
 import datetime, os, time, stat 
 
-
 now = datetime.datetime.now()
-
 
 #Class for print a file name  
 class DisplayFileName: 
-    def __inti__(name, number):
-    display.name = name 
-    display.number = number 
-    
-f = open('log.txt', 'a') #open an exiting file 
-f.write("Current time is : " + str(now) + "\n")
+    def __init__(self, fname):
+        self.fname = fname 
+        print(os.path.basename(os.path.basename(fname)))
 
+class LogToFile:
+    def __init__(self, ModTime):
+        self.ModTime = ModTime 
+        ModTime = time.ctime(FileCreated[stat.ST_MTIME])
+        f.write("Last Modified Time : " + ModTime + '\n\n')
+
+
+f = open('log.txt', 'a') #open an exiting file 
+#f.write("Current time is : " + str(now) + "\n")
 
 
 #Write a mod time of the log file
 FileCreated = os.stat('log.txt')
 FileName = 'log.txt'
-
-ModTime = time.ctime(FileCreated[stat.ST_MTIME])
+DisplayFileName(FileName)
+LogToFile
+#ModTime = time.ctime(FileCreated[stat.ST_MTIME])
 #f.write(FileCreated + "Last Modified Time: " + ModTime + '\n\n')
 
 #print(f.read())
@@ -31,8 +36,4 @@ ModTime = time.ctime(FileCreated[stat.ST_MTIME])
 
 f.write('\n')
 f.close()
-
-
-
-
 #logging.error('%s raised an error', output)
