a170f138db7a0e8567d5b7dc23a64a5104b83ad6
Akio <oriharaakio@yahoo.com>
2020-03-12 17:52:14 -0500

    a few changes to print file status

--- logging.py
+++ logging.py
@@ -16,24 +16,17 @@ class LogToFile:
 
 
 f = open('log.txt', 'a') #open an exiting file 
-#f.write("Current time is : " + str(now) + "\n")
-
+f.write("Current time is : " + str(now) + "\n")
 
 #Write a mod time of the log file
 FileCreated = os.stat('log.txt')
 FileName = 'log.txt'
-DisplayFileName(FileName)
-LogToFile
-#ModTime = time.ctime(FileCreated[stat.ST_MTIME])
-#f.write(FileCreated + "Last Modified Time: " + ModTime + '\n\n')
+#DisplayFileName(FileName)
+#LogToFile
+ModTime = time.ctime(FileCreated[stat.ST_MTIME])
+f.write(str(FileCreated) + "Last Modified Time: " + str(ModTime) + '\n')
 
 #print(f.read())
-
-
-
-
-
-
-f.write('\n')
+#f.write('\n')
 f.close()
 #logging.error('%s raised an error', output)
