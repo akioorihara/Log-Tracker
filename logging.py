5bfe95fa46ec42567c77b6a688ec824640b3d699
Akio <oriharaakio@yahoo.com>
2020-03-16 17:52:28 -0500

    for-loop will display files within the folder

--- logging.py
+++ logging.py
@@ -14,7 +14,9 @@ class LogToFile:
         f.write("Last Modified Time : " + ModTime + '\n\n')
 
 f = open('log.txt', 'a') #open an exiting file 
-f.write("Current time is : " + str(now) + "\n")
+
+#Write a current time 
+#f.write("Current time is : " + str(now) + "\n") 
 
 #Write a mod time of the log file
 FileCreated = os.stat('log.txt')
@@ -22,7 +24,18 @@ FileName = 'log.txt'
 #DisplayFileName(FileName)
 #LogToFile
 ModTime = time.ctime(FileCreated[stat.ST_MTIME])
-f.write(str(FileCreated) + "Last Modified Time: " + str(ModTime) + '\n\n')
+
+#f.write("Last Modified Time: " + (ModTime) + '\n' + "Path: " + os.path.realpath(__file__))
+#print("Current File Name: ", os.path.realpath(__file__))
+basepath = "."
+for entry in os.scandir(basepath):
+    if not entry.name.startswith('.') and entry.is_file():
+        print(entry.name)
+        #Check if oder than 7 years  
+        #I need to print a file name 
+        #I need to print a file size  
+
+    
 
 #print(f.read())
 #f.write('\n')
