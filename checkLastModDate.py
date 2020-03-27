0c8a881e8e0955db2a0102884fa7f437477bb2db
Akio <akio.orihara@outlook.com>
2020-03-26 19:04:52 -0500

    trying to figure out the ctime vs dateime

--- checkLastModDate.py
+++ checkLastModDate.py
@@ -1,20 +1,23 @@
 import datetime, os, time, stat, glob
 
+today = datetime.datetime.today()
 
 #Write a mod time of the log file
 fileCreated = os.stat('log.txt')
 #ModTime = time.ctime(FileCreated[stat.ST_MTIME])
 
 #current time 
-currentTime = time.ctime()
-today = datetime.datetime.today()
-file_mod_time = os.stat('log.txt').st_mtime
+currentTime = datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")
+
+#file_mod_time = os.stat('log.txt').st_mtime
 print(time.ctime(os.path.getmtime('log.txt')))
 
-d = datetime.datetime.today() - datetime.timedelta(hours=8760)
-#diff = today - file_mod_time
 
-print(today)
+d = datetime.datetime.today() - datetime.timedelta(hours=8760) #diff a year 
+#compare the differece 
+#diff = currentTime - file_mod_time
+
+print(currentTime)
 print(d)
 
 
